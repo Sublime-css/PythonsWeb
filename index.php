9232a626e8cfa3648967f069e38d76c267e2f869
Luka Simpson <31399112+Sublime-css@users.noreply.github.com>
2020-05-06 14:00:27 +1200

    Enabled multi-page database access

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <html lang="en">
 
 <head>
-    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -16,17 +15,16 @@
 
     <!--CSS reset, followed by CSS library, followed by site stylesheet (order is important)-->
     <link rel="stylesheet" href="css/reset.css" type="text/css" "media=screen">
+    <link rel="stylesheet" href="https://sublime-css.github.io/SublimeCSS/code/css/sublime.css">
     <link rel="stylesheet" href="css/style.css" type="text/css" "media=screen">
-    
 </head>
 
 <?php
 include "setup.php";
-$sql = "SELECT * FROM pages";
+$sql = "SELECT * FROM pages where pagenum = '3'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     $row = $result->fetch_assoc();
 } else {
     echo "ERROR: Nothing found";
@@ -35,43 +33,36 @@ $conn->close();
 ?>
 
 <body>
-    
     <header>
         <!--The header is a navigation bar in this design-->
         <nav>
             <!--There are two levels of navbar -- each level is a list of links-->
             <!--CSS is structured so only the list needs a class -- this prevents destroying other lists on the page-->
             <ul class="navbar">
-                <li><a href="index.html"><h1>Python's Web</h1></a></li>
-                <li><a href="index.html">Register/Login</a></li>
-                <li><a href="index.html">Share With Class</a></li>
-            </ul>    
-            
-            <ul class="navbar">
-                <li><a href="index.html">Search</a></li>
-                <li><a href="index.html">Coursework</a></li>
-                <li><a href="index.html">Donate</a></li>
-                <li><a href="index.html">News</a></li>
-                <li><a href="index.html">F. A. Q.</a></li>
-                <li><a href="index.html">Reference</a></li>
+                <li><a href="index.php"><h1>Python's Web</h1></a></li>
+                <li><a href="index.php">Register/Login</a></li>
+                <li><a href="index.php">Share With Class</a></li>
             </ul>
-            
+            <ul class="navbar">
+                <li><a href="index.php">Search</a></li>
+                <li><a href="index.php">Coursework</a></li>
+                <li><a href="index.php">Donate</a></li>
+                <li><a href="index.php">News</a></li>
+                <li><a href="index.php">F. A. Q.</a></li>
+                <li><a href="index.php">Reference</a></li>
+            </ul>
         </nav>
-
     </header>
     
     <!--The main body of the site, where content goes-->
     <div class="body">
-        
-        <!--PHP -- Here Be Dragons-->
+        <!--PHP - Here Be Dragons-->
         <?php echo $row["heading1"]?>
-        
+        <?php echo $row["text1"]?>
+        <!--End of Dragon's territory-->
     </div>
-    
     <footer>
-
+        <!--Whatever needs to go here:-->
     </footer>
-    
 </body>
-
 </html>
