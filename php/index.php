208268d73e5235685a5804f4cdb0990baf94a7a8
Luka Simpson <31399112+Sublime-css@users.noreply.github.com>
2020-08-10 11:21:43 +1200

    New UI beta

--- php/index.php
+++ php/index.php
@@ -4,24 +4,24 @@
         <?php include "head.php";?><!--Get the modular page header-->
     </head>
     <body>
-        
-        <div class="wrapper" style="width: 80vw">
         <?php include "nav.php";?><!--Get the modular navigation bar-->
-        <div class="body">
-            <?php include "dynamic.php"; //Get the modular content display system
-            //Some pages are special:
-            //Page 2 is the login page, display the login form there
-            if ($Page == "2"){
-                //Get the admin page with PHP because this isn't the dcocument's head
-                echo "\n<link rel=\"stylesheet\" href=\"../css/admin.css\" type=\"text/css\" \"media=screen\">\n";
-                //Get this if its not used already
-                require_once("setup_sec.php");
-                //Prepare to login
-                include "login/backEndLogin.php";
-                //Attempt login with user perms:
-                login("user");
-                } ?>
-        </div>
+            <div class="wrapper">
+                <?php include "dynamic.php"; //Get the modular content display system
+                //Some pages are special:
+                //Page 2 is the login page, display the login form there
+                if ($Page == "2"){
+                    //Get the admin page with PHP because this isn't the dcocument's head
+                    echo "\n<link rel=\"stylesheet\" href=\"../css/admin.css\" type=\"text/css\" \"media=screen\">\n";
+                    //Get this if its not used already
+                    require_once("setup_sec.php");
+                    //Prepare to login
+                    include "login/backEndLogin.php";
+                    //Attempt login with user perms:
+                    login("user");
+                    } ?>
+        <footer>
+            <p><?php include "../footer.txt"; ?></p>
+        </footer>
         </div>
     </body>
 </html>
