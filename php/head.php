8eaaa6f90ed045d2e07b8cc8fefc45d48b814ca5
Luka Simpson <31399112+Sublime-css@users.noreply.github.com>
2020-09-22 14:44:16 +1200

    css hotfixes and improvements
    Fix for the mobile menu z-index bug, and split up css for mobile/tablet into their own stylesheets.

--- php/head.php
+++ php/head.php
@@ -5,8 +5,10 @@
 <meta name="theme-color" content="#6e3bb0">
 <link rel="stylesheet" href="../css/reset.css" type="text/css" "media=screen">
 <link rel="stylesheet" href="../css/style.css" type="text/css" "media=screen">
-<script type="text/javascript" src="../js/mobile.js"></script>
+<script type="text/javascript" src="../js/mobile.js" media="screen and (max-width: 600px)"></script>
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon-16x16.png">
 <link rel="manifest" href="../site.webmanifest"><?php require_once("session.php");?>
+<link rel="stylesheet" type="text/css" href="../css/mobile.css" media="screen and (max-width: 600px)">
+<link rel="stylesheet" type="text/css" href="../css/tablet.css" media="screen and (min-width: 601px) and (max-width: 1200px)">
